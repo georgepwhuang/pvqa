09813404c9fdb3165f1a3defbb6564ef5f53dc1c
Huang Po-Wei <71061276+georgepwhuang@users.noreply.github.com>
2023-02-22 14:21:57 +0800

    Update Requirements and Add Setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pvqa',
+    version='',
+    packages=['pvqa', 'pvqa.metrics', 'pvqa.qencoders', 'pvqa.data_process', 'pvqa.classical_heads', 'examples'],
+    url='',
+    license='',
+    author='georgehuang',
+    author_email='',
+    description=''
+)
